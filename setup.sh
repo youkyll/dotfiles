70eb16abcfd9c0a2e7c6f2dedb40e43f81316bb7
youkyll <yuuki08180102@gmail.com>
2015-07-22 15:07:45 +0900

    Move sh script

--- setup.sh
+++ setup.sh
@@ -1,26 +1,51 @@
-#/bin/bash
+#/bin/sh
 
-function set_vimrc()
+set_vimrc()
 {
-	if [ -e ~/.vimrc ]
+	vimrc='~/.vimrc'
+	if [ -e $vimrc -a ! -L $vimrc ]
 	then
-		echo "mv vimrc"
-		mv ~/.vimrc ~/.vimrc_bu
+		echo 'mv vimrc'
+		mv $vimrc ~/.vimrc_bu
+	elif [ -L $vimrc ]
+	then
+		echo 'already symlink'
+		return 0
 	fi
 
-	ln -s ./vim/vimrc ~/.vimrc
+	ln -s ./vim/vimrc $vimrc
 }
 
-function set_bashrc()
+set_bashrc()
 {
-	if [ -e ~/.bashrc ]
+	bashrc='~/.bashrc'
+	if [ -e $bashrc -a ! -L $bashrc ]
 	then
-		echo "mv bashrc"
-		mv ~/.bashrc ~/.bashrc_bu
+		echo 'mv bashrc'
+		mv $bashrc ~/.bashrc_bu
+	elif [ -L $bashrc ]
+	then
+		echo 'already symlink'
+		return 0
 	fi
 
-	ln -s ./bash/bashrc ~/.bashrc
+	ln -s ./sh/bash/bashrc $bashrc
 }
 
+set_zshrc()
+{
+	zshrc='~/.zshrc'
+	if [ -e $zshrc -a ! -L $zshrc ]
+	then
+		echo 'mv zshrc'
+		mv $zshrc ~/.zshrc_bu
+	elif [ -L $zshrc ]
+	then
+		echo 'already symlink'
+		return 0
+	fi
+
+	ln -s ./sh/zsh/zshrc $zshrc
+}
 set_vimrc
 set_bashrc
