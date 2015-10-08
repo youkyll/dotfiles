6039e3e8231f022dd82c33ef9f67b84bed1f2582
youkyll <yuuki08180102@gmail.com>
2015-10-08 22:55:46 +0900

    Update load.sh for gcloud command

--- sh/common/load.sh
+++ sh/common/load.sh
@@ -5,3 +5,10 @@ if [ `uname` = "Darwin" ]; then
   export NVM_DIR=~/.nvm
   source $(brew --prefix nvm)/nvm.sh
 fi
+
+# The next line updates PATH for the Google Cloud SDK.
+source "$HOME/google-cloud-sdk/path.zsh.inc"
+
+# The next line enables shell command completion for gcloud.
+source "/Users/yuki/google-cloud-sdk/completion.zsh.inc"
+
