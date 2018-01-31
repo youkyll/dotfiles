108e3c9b12ac40012901a05f7a5ff4a52b771739
youkyll <yuuki08180102@gmail.com>
2018-02-01 02:22:26 +0900

    remove google cloud platform

--- sh/common/load.sh
+++ sh/common/load.sh
@@ -1,3 +1,4 @@
+
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
 
@@ -8,20 +9,13 @@ export NODE_PATH=${NVM_DIR:-$HOME/.nvm}/default/lib/node_modules
 NODE_PATH=${NODE_PATH:A}
 source "$HOME/dotfiles/sh/lib/nvm.sh"
 
-if [ -e $HOME/google-cloud-sdk/ ];then
-  # The next line updates PATH for the Google Cloud SDK.
-  source "$HOME/google-cloud-sdk/path.zsh.inc"
-
-  # The next line enables shell command completion for gcloud.
-  source "$HOME/google-cloud-sdk/completion.zsh.inc"
-fi
-
 export PATH="$HOME/local/bin:$PATH"
 
 # golang
 export GOPATH="$HOME/.go"
 export PATH="$PATH:$GOPATH/bin:$GOROOT/bin"
 
+echo 'stop'
 # ruby
 [[ -d ~/.rbenv  ]] && \
   export PATH=${HOME}/.rbenv/bin:${PATH} && \
